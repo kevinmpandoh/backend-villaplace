59c03d2eadcec6376c29c8714ce8d86b9ce094f1
Kevin Mclaren Pandoh <kevinmpandoh@gmail.com>
2024-11-27 16:43:48 +0800

    fix: menambahkan nama pembayar dan email pembayar

--- src/controllers/pembayaranController.ts
+++ src/controllers/pembayaranController.ts
@@ -6,7 +6,22 @@ const midtransClient = require("midtrans-client");
 const PembayaranController = {
   getAllPembayaran: async (req: Request, res: Response) => {
     try {
-      const pembayaran = await Pembayaran.find().populate("pesanan");
+      const pembayaran = await Pembayaran.find().populate([
+        {
+          path: "pesanan",
+          populate: [
+            {
+              path: "villa",
+              populate: [
+                {
+                  path: "foto_villa",
+                  model: "VillaPhoto",
+                },
+              ],
+            },
+          ],
+        },
+      ]);
 
       return res.status(200).json({
         status: "success",
@@ -24,9 +39,22 @@ const PembayaranController = {
 
   getPembayaranById: async (req: Request, res: Response) => {
     try {
-      const pembayaran = await Pembayaran.findById(req.params.id).populate(
-        "pesanan"
-      );
+      const pembayaran = await Pembayaran.findById(req.params.id).populate([
+        {
+          path: "pesanan",
+          populate: [
+            {
+              path: "villa",
+              populate: [
+                {
+                  path: "foto_villa",
+                  model: "VillaPhoto",
+                },
+              ],
+            },
+          ],
+        },
+      ]);
       if (!pembayaran) {
         return res.status(404).json({
           status: "error",
@@ -49,16 +77,112 @@ const PembayaranController = {
 
   createPembayaran: async (req: Request, res: Response) => {
     try {
+      const {
+        nama_pembayar,
+        email_pembayar,
+        kode_pembayaran,
+        status_pembayaran,
+        tanggal_pembayaran,
+        metode_pembayaran,
+        tipe_pembayaran,
+        jumlah_pembayaran,
+        expiry_time,
+        nomor_va,
+        pdf_url,
+      } = req.body;
+
+      const errors: Record<string, string> = {};
+
+      // switch (tipe_pembayaran) {
+      //   case "virtual_account":
+      //     if (!nomor_va) {
+      //       errors.nomor_va = "Nomor VA is required";
+      //     }
+      //     break;
+      //   case "credit_card":
+      //     break;
+      //   default:
+      //     errors.tipe_pembayaran = "Invalid payment type";
+      //     break;
+      // }
+
+      if (!nama_pembayar) {
+        errors.nama_pembayar = "Nama pembayar is required";
+      }
+
+      if (!email_pembayar) {
+        errors.email_pembayar = "Email pembayar is required";
+      }
+
+      if (!kode_pembayaran) {
+        errors.kode_pembayaran = "Kode pembayaran is required";
+      }
+
+      if (!status_pembayaran) {
+        errors.status_pembayaran = "Status pembayaran is required";
+      }
+
+      if (!tanggal_pembayaran) {
+        errors.tanggal_pembayaran = "Tanggal pembayaran is required";
+      }
+
+      if (!metode_pembayaran) {
+        errors.metode_pembayaran = "Metode pembayaran is required";
+      }
+
+      if (!tipe_pembayaran) {
+        errors.tipe_pembayaran = "Tipe pembayaran is required";
+      }
+
+      if (!jumlah_pembayaran) {
+        errors.jumlah_pembayaran = "Jumlah pembayaran is required";
+      }
+
+      if (!expiry_time) {
+        errors.expiry_time = "Expiry time is required";
+      }
+
+      if (!req.body.pesanan) {
+        errors.pesanan = "Pesanan is required";
+      }
+
       const pesanan = await Pesanan.findById(req.body.pesanan);
 
       if (!pesanan) {
-        return res.status(404).json({
+        errors.pesanan = "Pesanan not found";
+      }
+
+      // Cek jika kode pembayaran sudah ada
+      const kodePembayaranExist = await Pembayaran.findOne({
+        kode_pembayaran,
+      });
+
+      if (kodePembayaranExist) {
+        errors.kode_pembayaran = "Kode pembayaran already exist";
+      }
+
+      if (Object.keys(errors).length > 0) {
+        return res.status(400).json({
           status: "error",
-          message: "Pesanan not found",
+          message: "Bad request",
+          errors,
         });
       }
 
-      const newPembayaran = new Pembayaran(req.body);
+      const newPembayaran = new Pembayaran({
+        nama_pembayar,
+        email_pembayar,
+        kode_pembayaran,
+        status_pembayaran,
+        tanggal_pembayaran,
+        metode_pembayaran,
+        tipe_pembayaran,
+        jumlah_pembayaran,
+        expiry_time,
+        nomor_va,
+        pdf_url,
+        pesanan: req.body.pesanan,
+      });
 
       const savedPembayaran = await newPembayaran.save();
       return res.status(201).json({
@@ -77,9 +201,103 @@ const PembayaranController = {
 
   updatePembayaran: async (req: Request, res: Response) => {
     try {
+      const errors: Record<string, string> = {};
+
+      const {
+        nama_pembayar,
+        email_pembayar,
+        kode_pembayaran,
+        status_pembayaran,
+        tanggal_pembayaran,
+        metode_pembayaran,
+        tipe_pembayaran,
+        jumlah_pembayaran,
+        expiry_time,
+        nomor_va,
+        pdf_url,
+      } = req.body;
+
+      if (!nama_pembayar) {
+        errors.nama_pembayar = "Nama pembayar is required";
+      }
+
+      if (!email_pembayar) {
+        errors.email_pembayar = "Email pembayar is required";
+      }
+
+      if (!kode_pembayaran) {
+        errors.kode_pembayaran = "Kode pembayaran is required";
+      }
+
+      if (!status_pembayaran) {
+        errors.status_pembayaran = "Status pembayaran is required";
+      }
+
+      if (!tanggal_pembayaran) {
+        errors.tanggal_pembayaran = "Tanggal pembayaran is required";
+      }
+
+      if (!metode_pembayaran) {
+        errors.metode_pembayaran = "Metode pembayaran is required";
+      }
+
+      if (!tipe_pembayaran) {
+        errors.tipe_pembayaran = "Tipe pembayaran is required";
+      }
+
+      if (!jumlah_pembayaran) {
+        errors.jumlah_pembayaran = "Jumlah pembayaran is required";
+      }
+
+      if (!expiry_time) {
+        errors.expiry_time = "Expiry time is required";
+      }
+
+      if (!req.body.pesanan) {
+        errors.pesanan = "Pesanan is required";
+      }
+
+      if (req.body.pesanan) {
+        const pesanan = await Pesanan.findById(req.body.pesanan);
+
+        if (!pesanan) {
+          errors.pesanan = "Pesanan not found";
+        }
+      }
+
+      // cek apakah kode pembayaran sudah ada
+      const kodePembayaranExist = await Pembayaran.findOne({
+        kode_pembayaran,
+      });
+
+      if (kodePembayaranExist && kodePembayaranExist._id != req.params.id) {
+        errors.kode_pembayaran = "Kode pembayaran already exist";
+      }
+
+      if (Object.keys(errors).length > 0) {
+        return res.status(400).json({
+          status: "error",
+          message: "Bad request",
+          errors,
+        });
+      }
+
       const updatedPembayaran = await Pembayaran.findByIdAndUpdate(
         req.params.id,
-        req.body,
+        {
+          nama_pembayar,
+          email_pembayar,
+          kode_pembayaran,
+          status_pembayaran,
+          tanggal_pembayaran,
+          metode_pembayaran,
+          tipe_pembayaran,
+          jumlah_pembayaran,
+          expiry_time,
+          nomor_va,
+          pdf_url,
+          pesanan: req.body.pesanan,
+        },
         {
           new: true,
         }
@@ -130,6 +348,8 @@ const PembayaranController = {
 
   prosesPembayaran: async (req: Request, res: Response) => {
     try {
+      const errors: Record<string, string> = {};
+
       const {
         nama_pembayar,
         email_pembayar,
@@ -137,20 +357,29 @@ const PembayaranController = {
         jumlah_pembayaran,
       } = req.body;
 
-      if (
-        !nama_pembayar ||
-        !email_pembayar ||
-        !kode_pembayaran ||
-        !jumlah_pembayaran
-      ) {
+      if (!nama_pembayar) {
+        errors.nama_pembayar = "Nama pembayar is required";
+      }
+
+      if (!email_pembayar) {
+        errors.email_pembayar = "Email pembayar is required";
+      }
+
+      if (!kode_pembayaran) {
+        errors.kode_pembayaran = "Kode pembayaran is required";
+      }
+
+      if (!jumlah_pembayaran) {
+        errors.jumlah_pembayaran = "Jumlah pembayaran is required";
+      }
+
+      if (Object.keys(errors).length > 0) {
         return res.status(400).json({
           status: "error",
           message: "Bad request",
-          errors: "All fields are required",
+          errors,
         });
       }
-
-      // cek apakah
 
       const snap = new midtransClient.Snap({
         isProduction: false,
