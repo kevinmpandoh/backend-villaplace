e6719e9ace0a6f17bd80c53934da7d8062ba362b
Kevin Mclaren Pandoh <kevinmpandoh@gmail.com>
2024-12-18 10:44:17 +0800

    fix: menambahkan unit testing

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -58,6 +58,30 @@ describe("Authentication API Endpoints", () => {
       expect(response.body.data).toHaveProperty("email", userData.email);
     });
 
+    it("should return 400 if email is already registered", async () => {
+      const response = await request(app)
+        .post("/api/auth/user/register")
+        .send(userData);
+
+      expect(response.status).toBe(400);
+      expect(response.body.status).toBe("Failed");
+      expect(response.body.errors.email).toBe(
+        "Email sudah digunakan oleh user lain!"
+      );
+    });
+
+    it("should return 400 if no_teleon is already registered", async () => {
+      const response = await request(app)
+        .post("/api/auth/user/register")
+        .send({ ...userData, email: "user2@test.com" });
+
+      expect(response.status).toBe(400);
+      expect(response.body.status).toBe("Failed");
+      expect(response.body.errors.no_telepon).toBe(
+        "Nomor telepon sudah digunakan oleh user lain!"
+      );
+    });
+
     it("should login the user", async () => {
       const response = await request(app)
         .post("/api/auth/user/login")
@@ -67,6 +91,30 @@ describe("Authentication API Endpoints", () => {
       expect(response.body.status).toBe("Success");
       expect(response.body).toHaveProperty("token");
       userToken = response.body.token;
+    });
+
+    it("should return 400 if email was not found", async () => {
+      const response = await request(app)
+        .post("/api/auth/user/login")
+        .send({ email: "wrongemail@gmail.com", password: userData.password });
+
+      console.log(response.body);
+
+      expect(response.status).toBe(400);
+      expect(response.body.status).toBe("Failed");
+      expect(response.body.errors.email).toBe("Email tidak ditemukan!");
+    });
+
+    it("should return 400 if password is wrong", async () => {
+      const response = await request(app)
+        .post("/api/auth/user/login")
+        .send({ email: userData.email, password: "wrongpassword" });
+
+      expect(response.status).toBe(400);
+      expect(response.body.status).toBe("Failed");
+      expect(response.body.errors.password).toBe(
+        "Password yang Anda masukkan salah!"
+      );
     });
 
     it("should logout the user", async () => {
@@ -90,6 +138,30 @@ describe("Authentication API Endpoints", () => {
       expect(response.body.data).toHaveProperty("email", ownerData.email);
     });
 
+    it("should return 400 if email is already registered", async () => {
+      const response = await request(app)
+        .post("/api/auth/owner/register")
+        .send(ownerData);
+
+      expect(response.status).toBe(400);
+      expect(response.body.status).toBe("Failed");
+      expect(response.body.errors.email).toBe(
+        "Email sudah digunakan oleh owner lain!"
+      );
+    });
+
+    it("should return 400 if no_teleon is already registered", async () => {
+      const response = await request(app)
+        .post("/api/auth/owner/register")
+        .send({ ...ownerData, email: "owner2@test.com" });
+
+      expect(response.status).toBe(400);
+      expect(response.body.status).toBe("Failed");
+      expect(response.body.errors.no_telepon).toBe(
+        "Nomor telepon sudah digunakan oleh owner lain!"
+      );
+    });
+
     it("should login the owner", async () => {
       const response = await request(app)
         .post("/api/auth/owner/login")
@@ -99,6 +171,28 @@ describe("Authentication API Endpoints", () => {
       expect(response.body.status).toBe("Success");
       expect(response.body).toHaveProperty("token");
       ownerToken = response.body.token;
+    });
+
+    it("should return 400 if email was not found", async () => {
+      const response = await request(app)
+        .post("/api/auth/owner/login")
+        .send({ email: "wrongemail@gmail.com", password: ownerData.password });
+
+      expect(response.status).toBe(400);
+      expect(response.body.status).toBe("Failed");
+      expect(response.body.errors.email).toBe("Email tidak ditemukan!");
+    });
+
+    it("should return 400 if password is wrong", async () => {
+      const response = await request(app)
+        .post("/api/auth/owner/login")
+        .send({ email: ownerData.email, password: "wrongpassword" });
+
+      expect(response.status).toBe(400);
+      expect(response.body.status).toBe("Failed");
+      expect(response.body.errors.password).toBe(
+        "Password yang Anda masukkan salah!"
+      );
     });
 
     it("should logout the owner", async () => {
@@ -121,6 +215,26 @@ describe("Authentication API Endpoints", () => {
       expect(response.body.status).toBe("Success");
       expect(response.body).toHaveProperty("token");
       adminToken = response.body.token;
+    });
+
+    it("should return 400 if email was not found", async () => {
+      const response = await request(app)
+        .post("/api/auth/admin/login")
+        .send({ email: "wrongemail@gmail.com", password: "adminpassword123" });
+
+      expect(response.status).toBe(400);
+      expect(response.body.status).toBe("Failed");
+      expect(response.body.message).toBe("Email yang anda masukan salah");
+    });
+
+    it("should return 400 if password is wrong", async () => {
+      const response = await request(app)
+        .post("/api/auth/admin/login")
+        .send({ email: adminData.email, password: "wrongpassword" });
+
+      expect(response.status).toBe(400);
+      expect(response.body.status).toBe("Failed");
+      expect(response.body.message).toBe("Password yang anda masukan salah");
     });
 
     it("should logout the admin", async () => {
