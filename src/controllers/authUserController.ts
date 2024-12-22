eb28980d87f4efd6ad111e3a591cb5f0be5221d6
Kevin Mclaren Pandoh <kevinmpandoh@gmail.com>
2024-12-22 21:59:30 +0800

    fix: fix cookie

--- src/controllers/authUserController.ts
+++ src/controllers/authUserController.ts
@@ -154,7 +154,7 @@ export const loginUser = async (req: Request, res: Response): Promise<void> => {
       secure: true, // Hanya gunakan HTTPS
       sameSite: "none", // Atur SameSite: Strict
       maxAge: 24 * 60 * 60 * 1000, // 1 hari
-      domain: "frontend-villaplace.vercel.app",
+      domain: "https://frontend-villaplace.vercel.app",
     });
 
     res.json({
