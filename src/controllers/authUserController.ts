8d410ff571b457065421e93ec0a523025295567a
Kevin Mclaren Pandoh <kevinmpandoh@gmail.com>
2024-12-22 21:45:13 +0800

    fix: fix cookie

--- src/controllers/authUserController.ts
+++ src/controllers/authUserController.ts
@@ -154,7 +154,7 @@ export const loginUser = async (req: Request, res: Response): Promise<void> => {
       secure: true, // Hanya gunakan HTTPS
       sameSite: false, // Atur SameSite: Strict
       maxAge: 24 * 60 * 60 * 1000, // 1 hari
-      domain: "vercel.app",
+      domain: ".vercel.app",
     });
 
     res.json({
