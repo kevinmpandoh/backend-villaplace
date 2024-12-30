cfff60df1bd3c2add3962ae88094a9119371e34b
Kevin Mclaren Pandoh <kevinmpandoh@gmail.com>
2024-12-30 14:02:39 +0800

    fix bug

--- src/controllers/authUserController.ts
+++ src/controllers/authUserController.ts
@@ -152,7 +152,7 @@ export const loginUser = async (req: Request, res: Response): Promise<void> => {
 
     res.cookie("tokenUser", token, {
       secure: true, // Hanya gunakan HTTPS
-      sameSite: "lax", // Atur SameSite: Strict
+      sameSite: "lax",
       maxAge: 24 * 60 * 60 * 1000, // 1 hari
     });
 
